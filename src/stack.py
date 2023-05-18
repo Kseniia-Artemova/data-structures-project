fd39a7bd1dc89792e47fabf88acaf728a543a243
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-05-18 03:22:09 +0200

    modified attributes classes Stack and Node, fixed tests for their methods (#4)

--- src/stack.py
+++ src/stack.py
@@ -1,25 +1,48 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node=None):
+    def __init__(self, data: str) -> None:
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
+        :param next_node: ссылка на следующий объект стека, либо None
         """
+
         self.data = data
-        self.next_node = next_node
+        self.__next_node = None
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, next_node):
+        self.__next_node = next_node
 
 
 class Stack:
     """Класс для стека"""
 
     def __init__(self):
-        """Конструктор класса Stack"""
-        self.items = []
-        self.top = None
+        """
+        Конструктор класса Stack
 
-    def push(self, data):
+        :param items: список добавленных узлов
+        :param top: ссылка на последний объект стека, либо None
+        """
+        self.__items = []
+        self.__top = None
+
+    @property
+    def items(self) -> list:
+        return self.__items
+
+    @property
+    def top(self) -> Node | None:
+        return self.__top
+
+    def push(self, data: str) -> None:
         """
         Метод для добавления элемента на вершину стека
 
@@ -27,21 +50,21 @@ class Stack:
         """
         data_node = Node(data)
 
-        if self.items:
-            data_node.next_node = self.items[-1]
+        if self.__items:
+            data_node.next_node = self.__items[-1]
 
-        self.items.append(data_node)
-        self.top = data_node
+        self.__items.append(data_node)
+        self.__top = data_node
 
-    def pop(self):
+    def pop(self) -> str | None:
         """
         Метод для удаления элемента с вершины стека и его возвращения
 
         :return: данные удаленного элемента
         """
-        if self.items:
-            last_item = self.items.pop()
-            self.top = self.items[-1] if self.items else None
-            return last_item
+        if self.__items:
+            last_item = self.__items.pop()
+            self.__top = self.__items[-1] if self.__items else None
+            return last_item.data
         else:
             raise IndexError("pop from empty list")
