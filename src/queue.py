a7e1c14d4549def1f8b92f22fa79ed091a2d28bf
Kseniia <127607406+Kseniia-Artemova@users.noreply.github.com>
2023-05-23 04:04:16 +0200

    Homework 3 (#6)
    * added method __str__ and tests for it
    
    * added methods of class queue, added tests for it
    
    * small fixes
    
    * corrected class Stack and its methods, tests fixed

--- src/queue.py
+++ src/queue.py
@@ -1,38 +1,93 @@
 class Node:
     """Класс для узла очереди"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data: str, next_node=None) -> None:
         """
         Конструктор класса Node
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+
+        self.data = data
+        self.__next_node = next_node
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, next_node):
+        self.__next_node = next_node
+
+    def __str__(self) -> str:
+        """Возвращает строку data класса в формате строки"""
+
+        return f"{self.data}"
 
 
 class Queue:
     """Класс для очереди"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         """Конструктор класса Queue"""
-        pass
 
-    def enqueue(self, data):
+        self.__head = None
+        self.__tail = None
+
+    @property
+    def head(self) -> Node | None:
+        return self.__head
+
+    @property
+    def tail(self) -> Node | None:
+        return self.__tail
+
+    def enqueue(self, data: str) -> None:
         """
         Метод для добавления элемента в очередь
 
         :param data: данные, которые будут добавлены в очередь
         """
-        pass
 
-    def dequeue(self):
+        node = Node(data)
+
+        if self.head is None:
+            self.__head = node
+        else:
+            self.tail.next_node = node
+
+        self.__tail = node
+
+    def dequeue(self) -> str:
         """
         Метод для удаления элемента из очереди. Возвращает данные удаленного элемента
 
         :return: данные удаленного элемента
         """
-        pass
 
-    def __str__(self):
+        if self.head is None:
+            raise IndexError("Очередь пуста")
+
+        else:
+            node = self.head.data
+
+            if self.head.next_node is None:
+                self.__tail = self.__head = None
+            else:
+                self.__head = self.head.next_node
+
+            return node
+
+    def __str__(self) -> str:
         """Магический метод для строкового представления объекта"""
-        pass
+
+        if self.head is None:
+            return ""
+
+        nodes = ""
+        node = self.head
+        while node:
+            nodes += node.data + "\n"
+            node = node.next_node
+
+        return nodes.rstrip("\n")
--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,77 @@
 """Здесь надо написать тесты с использованием unittest для модуля queue."""
+
+from src.queue import Queue
+from src.queue import Node
+import unittest
+
+
+class TestNode(unittest.TestCase):
+
+    def test_create_node(self):
+        node = Node("data1")
+        self.assertEqual(node.data, "data1")
+        self.assertIsNone(node.next_node)
+
+    def test_str_node(self):
+        node = Node("data1")
+        self.assertEqual(str(node), "data1")
+
+
+class TestQueue(unittest.TestCase):
+
+    def test_create_queue_empty(self):
+        queue = Queue()
+        self.assertIsNone(queue.head)
+        self.assertIsNone(queue.tail)
+
+    def test_enqueue(self):
+        queue = Queue()
+        queue.enqueue("data1")
+        self.assertEqual(queue.head.data, "data1")
+        self.assertEqual(queue.tail.data, "data1")
+        self.assertIsNone(queue.head.next_node)
+        self.assertIsNone(queue.tail.next_node)
+
+        queue.enqueue("data2")
+        self.assertEqual(queue.head.data, "data1")
+        self.assertEqual(queue.tail.data, "data2")
+        self.assertEqual(queue.head.next_node.data, "data2")
+        self.assertIsNone(queue.tail.next_node)
+
+    def test_dequeue(self):
+        queue = Queue()
+        queue.enqueue("data1")
+        queue.enqueue("data2")
+        queue.enqueue("data3")
+
+        last = queue.dequeue()
+        self.assertEqual(last, "data1")
+        self.assertEqual(queue.head.data, "data2")
+        self.assertEqual(queue.head.next_node.data, "data3")
+        self.assertIsNone(queue.tail.next_node)
+
+        last = queue.dequeue()
+        self.assertEqual(last, "data2")
+        self.assertEqual(queue.head.data, "data3")
+        self.assertIsNone(queue.head.next_node)
+
+        last = queue.dequeue()
+        self.assertEqual(last, "data3")
+        self.assertIsNone(queue.head)
+        self.assertIsNone(queue.tail)
+
+        with self.assertRaises(IndexError):
+            queue.dequeue()
+
+    def test_str_empty(self):
+        queue = Queue()
+        self.assertEqual(str(queue), "")
+
+    def test_str(self):
+        queue = Queue()
+        queue.enqueue("data1")
+        queue.enqueue("data2")
+        queue.enqueue("data3")
+
+        self.assertEqual(str(queue), "data1\ndata2\ndata3")
+
